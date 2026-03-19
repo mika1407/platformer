3b6b36e52d6cdd62909cc52ad6126a72b5f6e057
mika1407 <koneautomaatio@hotmail.com>
2026-03-19 15:14:11 +0200

    run added

--- tutorial.py
+++ tutorial.py
@@ -90,9 +90,20 @@ class Player(pygame.sprite.Sprite):
         self.move(self.x_vel, self.y_vel)
 
         self.fall_count += 1
-    
+        self.update_sprite()
+
+    def update_sprite(self):
+        sprite_sheet = "idle"
+        if self.x_vel != 0:
+            sprite_sheet = "run"
+
+        sprite_sheet_name = sprite_sheet + "_" + self.direction
+        sprites = self.SPRITES[sprite_sheet_name]
+        sprite_index = (self.animation_count // self.ANIMATION_DELAY) % len(sprites)  
+        self.sprite = sprites[sprite_index]
+        self.animation_count += 1
+
     def draw(self, win):
-        self.sprite = self.SPRITES["idle_" + self.direction][0]
         win.blit(self.sprite, (self.rect.x, self.rect.y))
 
 def get_background(name):
